e08437b24206b4153dc078d54799b93629b3f1e1
Carlos <carlosacia2@gmail.com>
2025-07-02 19:34:39 +0200

    [ADD] Functions to create more data with probabilities

--- create_db/process_db.py
+++ create_db/process_db.py
@@ -1,5 +1,7 @@
 # Created by Carlos Alvarez Cia on 02-07-2025
 import pandas as pd
+import json
+import numpy as np
 
 def filter_three_days(real_data):
     """ Get the first three days of data from the real data. Filter from 05-06-25 to 08-06-25.
@@ -75,3 +77,110 @@ def split_by_day(data):
     """
     data['date'] = data['timestamp'].dt.date
     return [group for _, group in data.groupby('date')]
+
+
+def compute_probability_factor(events_probabilities):
+    """ Compute the probability factor based on the events probabilities.
+    
+    Args:
+        events_probabilities (dict): A dictionary containing the probabilities and factors for each event.
+    
+    Returns:
+        float: The computed probability factor.
+    """
+    factor = 1.0  # Start with a factor of 1 (no change)
+    events = []  # List to store the events that occurred
+    
+    for event in events_probabilities:
+        occurred = 0
+        _, event_info = list(event.items())[0] 
+                
+        if np.random.rand() < event_info.get('probability', 0):
+            factor += event_info.get('impact', 0)
+            occurred = 1
+
+        events.append(occurred)
+
+    return factor, events
+
+
+
+
+def create_more_data(daily_data, weeks=4):
+    """ Create more data for the specified number of weeks.
+    
+    Args:
+        daily_data (list): A list of DataFrames, each containing data for one day (thursday, friday, saturday).
+        weeks (int): The number of weeks to create data for.
+    
+    Returns:
+        pd.DataFrame: The complete data DataFrame with additional weeks of data.
+    """
+    complete_data = pd.DataFrame()
+
+    workday = daily_data[0]  # Thursday data
+    friday = daily_data[1]  # Friday data
+    weekend = daily_data[2]  # Saturday data
+
+    # Create the data from the first monday of june 2025
+    initial_timestamp = pd.to_datetime('2025-06-02 00:00:00')
+
+    with open('create_db/probabilities.json', 'r') as file:
+        events_probabilities = json.load(file)['binary_events']
+
+    for i in range(weeks):
+        for day in range(7):
+            factor, events = compute_probability_factor(events_probabilities)
+
+            match day:
+                # For workdays (Monday to Thursday)
+                case 0 | 1 | 2 | 3 :  
+                    day_data = workday.copy()
+                    day_data['timestamp'] = initial_timestamp + pd.Timedelta(days=i * 7 + day)
+                    # Save the size of station before applying the factor
+                    station_size = day_data['available_bikes'] + day_data['available_stands']
+                    # Apply the factor to the difference in available bikes
+                    day_data['available_bikes'] = (day_data['available_bikes'] * factor).astype(int)
+                    # Ensure the available bikes do not exceed the station size
+                    day_data['available_bikes'] = day_data['available_bikes'].clip(upper=station_size)
+                    # Update the available stands accordingly
+                    day_data['available_stands'] = station_size - day_data['available_bikes']
+                    for event_dict, occurred in zip(events_probabilities, events):
+                        event_name, _ = list(event_dict.items())[0]
+                        day_data[event_name] = occurred
+                    
+                # For Friday
+                case 4:
+                    day_data = friday.copy()
+                    day_data['timestamp'] = initial_timestamp + pd.Timedelta(days=i * 7 + day)
+                    # Save the size of station before applying the factor
+                    station_size = day_data['available_bikes'] + day_data['available_stands']
+                    # Apply the factor to the difference in available bikes
+                    day_data['available_bikes'] = (day_data['available_bikes'] * factor).astype(int)
+                    # Ensure the available bikes do not exceed the station size
+                    day_data['available_bikes'] = day_data['available_bikes'].clip(upper=station_size)
+                    # Update the available stands accordingly
+                    day_data['available_stands'] = station_size - day_data['available_bikes']
+                    for event_dict, occurred in zip(events_probabilities, events):
+                        event_name, _ = list(event_dict.items())[0]
+                        day_data[event_name] = occurred
+
+                # For Saturday and Sunday
+                case 5 | 6 :
+                    day_data = weekend.copy()
+                    day_data['timestamp'] = initial_timestamp + pd.Timedelta(days=i * 7 + day)
+                    # Save the size of station before applying the factor
+                    station_size = day_data['available_bikes'] + day_data['available_stands']
+                    # Apply the factor to the difference in available bikes
+                    day_data['available_bikes'] = (day_data['available_bikes'] * factor).astype(int)
+                    # Ensure the available bikes do not exceed the station size
+                    day_data['available_bikes'] = day_data['available_bikes'].clip(upper=station_size)
+                    # Update the available stands accordingly
+                    day_data['available_stands'] = station_size - day_data['available_bikes']
+                    for event_dict, occurred in zip(events_probabilities, events):
+                        event_name, _ = list(event_dict.items())[0]
+                        day_data[event_name] = occurred
+            
+            complete_data = pd.concat([complete_data, day_data], ignore_index=True)
+    
+    return complete_data
---
+++ create_db/test.py
@@ -0,0 +1,21 @@
+import get_data_from_postgresql
+import process_db
+
+
+if __name__ == "__main__":
+    print("This is the test module of the project.")
+    
+    # Fetch real data from the PostgreSQL database
+    db_config = get_data_from_postgresql.get_db_config()
+    real_data = get_data_from_postgresql.get_real_data(db_config)
+    
+    # Process the fetched data
+    processed_data = process_db.filter_three_days(real_data)
+    processed_data = process_db.fill_gaps_with_synthetic_data(processed_data)
+    processed_data = process_db.split_by_day(processed_data)
+    processed_data = process_db.create_more_data(processed_data,3)
+    processed_data.to_csv('processed_data.csv', index=False)  # Save the processed data to a CSV file
+    
+    # Display the first few rows of the processed data
+    print("Processed Data:")
+    print(processed_data.head())  # Display the first few rows of the processed data
