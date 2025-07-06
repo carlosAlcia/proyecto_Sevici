cfe81f7f6e3864b8fe53acc13cd9a63e31f2f09d
Carlos <carlosacia2@gmail.com>
2025-07-06 19:41:23 +0200

    [NEW|DEL] Script to generate DB. Deleted test script.

--- create_db/generate_db.py
+++ create_db/generate_db.py
@@ -1,9 +1,12 @@
+# Created by Carlos Alvarez on 06-07-2025
+
 import get_data_from_postgresql
 import process_db
 
 
 if __name__ == "__main__":
-    print("This is the test module of the project.")
+
+    TOTAL_MONTHS = 4
     
     # Fetch real data from the PostgreSQL database
     db_config = get_data_from_postgresql.get_db_config()
@@ -13,10 +16,6 @@ if __name__ == "__main__":
     processed_data = process_db.filter_three_days(real_data)
     processed_data = process_db.fill_gaps_with_synthetic_data(processed_data)
     processed_data = process_db.split_by_day(processed_data)
-    processed_data = process_db.create_more_data(processed_data,12)
-    processed_data.to_csv('processed_data.csv', index=False, date_format='%d-%m-%Y-%H-%M-%S')  # Save the processed data to a CSV file
+    processed_data = process_db.create_more_data(processed_data,4*TOTAL_MONTHS)
+    processed_data.to_csv('../prediction/dataset.csv', index=False, date_format='%d-%m-%Y-%H-%M-%S')  # Save the processed data to a CSV file
       
-
-    # Display the first few rows of the processed data
-    print("Processed Data:")
-    print(processed_data.head())  # Display the first few rows of the processed data
